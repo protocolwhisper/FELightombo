5dbd56cde4e135286abc561bc17202f07a8cf9a1
ProtocolWhisper <chrismatai@hotmail.com>
2026-02-02 13:32:22 +0800

    (fix): vercel error

--- components/AsciiBackground.tsx
+++ components/AsciiBackground.tsx
@@ -17,7 +17,7 @@ interface Particle {
 export default function AsciiBackground() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const particlesRef = useRef<Particle[]>([]);
-  const animationRef = useRef<number>();
+  const animationRef = useRef<number | undefined>(undefined);
 
   useEffect(() => {
     const canvas = canvasRef.current;
